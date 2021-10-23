d045ce87add5ffad98d3fb13b72693ce05eade02
kevinRepos-droid <acelite27@gmail.com>
2021-10-22 20:15:47 -0600

    Crear Convertidor

--- convertir/src/components/Convertidor.tsx
+++ convertir/src/components/Convertidor.tsx
@@ -4,38 +4,37 @@ import {View, Text, TextInput,StyleSheet,Button} from 'react-native'
 export const Convertidor = () => {
     return (
          <View>
-        <Text style={styles.texto}> Origen</Text>
+        <Text style={styles.texto}> Monto_1</Text>
         <TextInput style={styles.input}/>
 
-        <Text style={styles.texto}> Destino</Text>
+        <Text style={styles.texto}> Monto_2</Text>
         <TextInput style={styles.input}/>
 
-        <Text style={styles.texto}> Convertir</Text>
-        <TextInput style={styles.input}/>
-
-        <Text style={styles.texto}> Resultado</Text>
-        <TextInput style={styles.input}/>
+          
       
         <View style={styles.button}>
         <Button 
-            title="calcular"
-            onPress={()=> alert('hola')}
+            title="Convertir"
+            onPress={()=> alert('Convirtiendo')}
             />
             </View>
+            <Text style={styles.texto}> Resultado</Text>
+        <TextInput style={styles.input}/>
           </View>
     )
 }
 const styles = StyleSheet.create({
     button:{
         marginTop: 10,
+        color:'red'
     },
     input:{
         borderWidth: 1,
         borderRadius: 5,
         paddingHorizontal:80,
         paddingVertical:5,
-        borderColor: 'green',
-        backgroundColor:'gray'
+        borderColor: 'red',
+        backgroundColor:'white'
     },
     texto:{
         textAlign: 'center'
